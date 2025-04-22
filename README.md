4ae25f8d7ecfba3248e0a558d3ed068e967619bb
Eduardo H. da Silva <27dhiouz9@mozmail.com>
2025-04-22 15:41:43 -0300

    Initial

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-# php-simple-template
+# languages-gradient
 
 ![PHP](https://img.shields.io/badge/php-%23777BB4.svg?style=for-the-badge&logo=php&logoColor=white)
 ![Visual Studio Code](https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white)
+
+
+![Test](http://localhost:8000/?owner=eduhds&repo=gspm)
 
 PHP template for simple projects.
 
--- src/index.php
+++ src/index.php
@@ -1,4 +1,78 @@
 <?php
-__DIR__ . '/../vendor/autoload.php';
 
-echo 'Hello World';
+function svgGradient($values, $height = 15)
+{
+  $svg =
+    '<svg height="' .
+    $height .
+    '" width="100%" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient id="grad1" x1="0%" x2="100%" y1="0%" y2="0%">';
+
+  $accumulator = 0;
+
+  foreach ($values as $key => $value) {
+    $accumulator += $value['percent'];
+
+    $offset = (int) match ($key) {
+      0 => 0,
+      count($values) - 1 => 100,
+      default => $accumulator
+    };
+
+    $svg .=
+      '<stop offset="' . $offset . '%" stop-color="' . $value['color'] . '" />';
+  }
+
+  $svg .=
+    '</linearGradient></defs><rect width="100%" height="' .
+    $height .
+    '" x="0" y="0" fill="url(#grad1)" /></svg>';
+
+  return $svg;
+}
+
+if (isset($_GET['owner']) && isset($_GET['repo'])) {
+  $owner = $_GET['owner'];
+  $repo = $_GET['repo'];
+
+  $res = file_get_contents(
+    "https://api.github.com/repos/$owner/$repo/languages",
+    false,
+    stream_context_create([
+      'http' => [
+        'method' => 'GET',
+        'header' =>
+          'Accept: application/vnd.github.v3+json' .
+          PHP_EOL .
+          'User-Agent: svg-colors/1.0' .
+          PHP_EOL
+      ]
+    ])
+  );
+
+  $repoLanguages = json_decode($res, true);
+
+  $infoLanguages = json_decode(file_get_contents('languages.json'), true);
+
+  $total = array_sum(array_values($repoLanguages));
+  $values = [];
+
+  foreach ($repoLanguages as $key => $value) {
+    $percent = ($value / $total) * 100;
+    if ($percent <= 0) {
+      continue;
+    }
+    $values[] = [
+      'percent' => $percent,
+      'color' => $infoLanguages[$key]['color']
+    ];
+  }
+} else {
+  $values = [
+    ['percent' => 50, 'color' => '#ff0000'],
+    ['percent' => 50, 'color' => '#00ff00'],
+    ['percent' => 50, 'color' => '#0000ff']
+  ];
+}
+
+header('Content-Type: image/svg+xml');
+echo svgGradient($values);
